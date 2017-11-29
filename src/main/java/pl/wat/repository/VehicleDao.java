c50d9e0c3d33e78a6911e2280ef4999dae6a1a4f
JZareba <kuba.zrb@gmail.com>
2017-11-29 20:17:43 +0100

    commit

--- src/main/java/pl/wat/repository/VehicleDao.java
+++
@@ -1,9 +0,0 @@
-package pl.wat.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-import pl.wat.model.Vehicle;
-
-@Repository
-public interface VehicleDao extends CrudRepository<Vehicle, Long> {
-}
--- src/main/java/pl/wat/repository/VehicleOwnerDao.java
+++
@@ -1,9 +0,0 @@
-package pl.wat.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-import pl.wat.model.VehicleOwner;
-
-@Repository
-public interface VehicleOwnerDao extends CrudRepository<VehicleOwner, Long> {
-}
